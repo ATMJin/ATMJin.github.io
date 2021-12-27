574658ee423889e72801361f2159e924d7d1f40e
ATMJin <atmjin0912@protonmail.com>
2021-12-27 09:25:20 +0800

    第一次報告前_改，增加文字輪播

--- personal_project/script/front.script.js
+++ personal_project/script/front.script.js
@@ -1,37 +1,60 @@
 function pre_pic() {
   console.log("error")
   let pic01 = document.querySelector(".course_image.show");
+  let paragraph01 = document.querySelector(".course_introduce_words.show")
   let pic02 = document.querySelector(".course_image.hidden.hidden_left");
+  let paragraph02 = document.querySelector(".course_introduce_words.hidden.hidden_left")
   let pic03 = document.querySelector(".course_image.hidden.hidden_right");
+  let paragraph03 = document.querySelector(".course_introduce_words.hidden.hidden_right")
+
 
   pic01.classList.remove("show");
   pic01.classList.add("hidden");
   pic01.classList.add("hidden_right");
+  paragraph01.classList.remove("show");
+  paragraph01.classList.add("hidden");
+  paragraph01.classList.add("hidden_right");
 
   pic03.classList.remove("hidden_right");
   pic03.classList.add("hidden_left");
+  paragraph03.classList.remove("hidden_right");
+  paragraph03.classList.add("hidden_left");
 
   pic02.classList.remove("hidden");
   pic02.classList.remove("hidden_left");
   pic02.classList.add("show");
+  paragraph02.classList.remove("hidden");
+  paragraph02.classList.remove("hidden_left");
+  paragraph02.classList.add("show");
 }
 
 
 function next_pic() {
   let pic01 = document.querySelector(".course_image.show");
+  let paragraph01 = document.querySelector(".course_introduce_words.show")
   let pic02 = document.querySelector(".course_image.hidden.hidden_left");
+  let paragraph02 = document.querySelector(".course_introduce_words.hidden.hidden_left")
   let pic03 = document.querySelector(".course_image.hidden.hidden_right");
+  let paragraph03 = document.querySelector(".course_introduce_words.hidden.hidden_right")
 
   pic01.classList.remove("show");
   pic01.classList.add("hidden");
   pic01.classList.add("hidden_left");
+  paragraph01.classList.remove("show");
+  paragraph01.classList.add("hidden");
+  paragraph01.classList.add("hidden_left");
 
   pic02.classList.remove("hidden_left");
   pic02.classList.add("hidden_right");
+  paragraph02.classList.remove("hidden_left");
+  paragraph02.classList.add("hidden_right");
 
   pic03.classList.remove("hidden");
   pic03.classList.remove("hidden_right");
   pic03.classList.add("show");
+  paragraph03.classList.remove("hidden");
+  paragraph03.classList.remove("hidden_right");
+  paragraph03.classList.add("show");
 }
 
 
@@ -43,4 +66,4 @@ function init() {
 
 
 window.addEventListener("load", init, false);
-// setInterval(next_pic, 1500)
+setInterval(next_pic, 5000)
